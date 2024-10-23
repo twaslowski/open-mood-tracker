2671a2bbd6ef1eb3a256ab2a527b03f802e0aec1
Tobias Waslowski <tobiaswaslowski@gmail.com>
2024-10-23 21:02:56 +0200

    feat: only create new records if no incomplete ones exist

--- src/main/java/de/twaslowski/moodtracker/adapter/telegram/handler/RecordHandler.java
+++ src/main/java/de/twaslowski/moodtracker/adapter/telegram/handler/RecordHandler.java
@@ -4,6 +4,7 @@ import de.twaslowski.moodtracker.adapter.telegram.dto.response.TelegramInlineKey
 import de.twaslowski.moodtracker.adapter.telegram.dto.response.TelegramResponse;
 import de.twaslowski.moodtracker.adapter.telegram.dto.update.TelegramUpdate;
 import de.twaslowski.moodtracker.adapter.telegram.handler.callback.CallbackGenerator;
+import de.twaslowski.moodtracker.entity.Record;
 import de.twaslowski.moodtracker.service.RecordService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,8 +23,13 @@ public class RecordHandler implements UpdateHandler {
   @Override
   public TelegramResponse handleUpdate(TelegramUpdate update) {
     log.info("{}: Handling record command.", update.getChatId());
-    // todo there is currently nothing stopping a user from creating multiple records
-    // simply add a check for existing records when /record is sent
+    var existingRecord = recordService.findIncompleteRecordForTelegramChat(update.getChatId());
+
+    return existingRecord.map(record -> handleExistingIncompleteRecord(update, record))
+        .orElseGet(() -> createNewRecord(update));
+  }
+
+  private TelegramInlineKeyboardResponse createNewRecord(TelegramUpdate update) {
     var record = recordService.initializeFrom(update);
     var firstMetric = recordService.getNextIncompleteMetric(record)
         .orElseThrow(() -> new IllegalStateException("No empty metrics found for record after initialization."));
@@ -33,6 +39,20 @@ public class RecordHandler implements UpdateHandler {
         .chatId(update.getChatId())
         .content(callbackGenerator.createCallbacks(firstMetric))
         .text(firstMetric.getChatPrompt())
+        .build();
+  }
+
+  private TelegramInlineKeyboardResponse handleExistingIncompleteRecord(TelegramUpdate update, Record record) {
+    log.info("Found existing record for chatId {}: {}", update.getChatId(), record.getId());
+
+    // Since we know there is an incomplete record, we can safely call get()
+    // noinspection OptionalGetWithoutIsPresent
+    var nextMetric = recordService.getNextIncompleteMetric(record).get();
+
+    return TelegramInlineKeyboardResponse.builder()
+        .chatId(update.getChatId())
+        .content(callbackGenerator.createCallbacks(nextMetric))
+        .text("You're already recording. Please complete the current metric.\n\n" + nextMetric.getChatPrompt())
         .build();
   }
 
---
+++ src/test/java/de/twaslowski/moodtracker/adapter/telegram/handler/RecordHandlerTest.java
@@ -0,0 +1,65 @@
+package de.twaslowski.moodtracker.adapter.telegram.handler;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import de.twaslowski.moodtracker.adapter.telegram.dto.update.TelegramTextUpdate;
+import de.twaslowski.moodtracker.adapter.telegram.handler.callback.CallbackGenerator;
+import de.twaslowski.moodtracker.entity.Record;
+import de.twaslowski.moodtracker.entity.metric.Mood;
+import de.twaslowski.moodtracker.service.RecordService;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class RecordHandlerTest {
+
+  @InjectMocks
+  private RecordHandler recordHandler;
+
+  @Mock
+  private RecordService recordService;
+
+  @Mock
+  private CallbackGenerator callbackGenerator;
+
+  @Test
+  void shouldCreateNewRecordIfNoIncompleteRecordExists() {
+    var update = TelegramTextUpdate.builder()
+        .text("/record")
+        .chatId(1L).build();
+
+    when(recordService.findIncompleteRecordForTelegramChat(1L)).thenReturn(Optional.empty());
+
+    // Without these, we run into a bunch of NullPointers
+    when(recordService.getNextIncompleteMetric(any())).thenReturn(Optional.of(new Mood()));
+    when(recordService.initializeFrom(update)).thenReturn(Record.builder().id(1L).build());
+
+    recordHandler.handleUpdate(update);
+
+    verify(recordService).initializeFrom(update);
+  }
+
+  @Test
+  void shouldHandleExistingIncompleteRecord() {
+    var update = TelegramTextUpdate.builder()
+        .text("/record")
+        .chatId(1L).build();
+
+    var record = Record.builder().id(1L).build();
+    when(recordService.findIncompleteRecordForTelegramChat(1L)).thenReturn(Optional.of(record));
+
+    // Without these, we run into a bunch of NullPointers
+    when(recordService.getNextIncompleteMetric(record)).thenReturn(Optional.of(new Mood()));
+
+    recordHandler.handleUpdate(update);
+
+    verify(recordService, never()).initializeFrom(any());
+  }
+}
