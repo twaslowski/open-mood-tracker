153ccb46be36f1658dcc0ebb46578333adae48e4
Tobias Waslowski <tobiaswaslowski@gmail.com>
2024-10-31 21:30:05 +0200

    feat: working helm setup for running application

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM amazoncorretto:21-alpine
 HEALTHCHECK --interval=30s --timeout=3s \
   CMD wget --no-verbose -q --tries=1 --no-check-certificate https://localhost:8443/actuator/health || exit 1
 
+ENV SPRING_PROFILES_ACTIVE=prod
 ENV APP_DIR=/var/opt
 
 RUN mkdir -p $APP_DIR
@@ -11,4 +12,4 @@ COPY target/mood-tracker-1.0-SNAPSHOT.jar ${APP_DIR}/mood-tracker.jar
 
 EXPOSE 8443
 
-CMD ["java", "-jar", "/var/opt/mood-tracker.jar"]
+CMD ["java", "-jar", "/var/opt/mood-tracker.jar", "-Dspring.profiles.active=${SPRING_PROFILES_ACTIVE}"]
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,44 @@
 resource "helm_release" "postgres" {
-  name             = "postgres"
-  repository       = "oci://registry-1.docker.io/bitnamicharts"
-  chart            = "postgresql"
-  namespace        = "open-mood-tracker"
+  name = "postgres"
+  repository = "oci://registry-1.docker.io/bitnamicharts"
+  chart = "postgresql"
+  namespace = local.namespace
   create_namespace = true
+
+  values = [
+    <<YAML
+    global:
+      postgresql:
+        auth:
+          password: some-password
+          username: some-user
+          database: mood-tracker
+    nameOverride: postgres
+  YAML
+  ]
 }
 
 resource "helm_release" "application" {
-  chart     = "../charts/open-mood-tracker"
-  name      = "open-mood-tracker"
+  chart = "../charts/open-mood-tracker"
+  name = "open-mood-tracker"
   namespace = "open-mood-tracker"
 
-  values = [<<YAML
+  values = [
+    <<YAML
     environmentVariables:
       - name: SPRING_PROFILES_ACTIVE
         value: prod
       - name: TELEGRAM_TOKEN
         value: ${var.telegram_token}
+      - name: DATASOURCE_URL
+        value: "jdbc:postgresql://postgres.${local.namespace}.svc.cluster.local:5432/mood-tracker"
+      - name: DATASOURCE_USERNAME
+        value: some-user
+      - name: DATASOURCE_PASSWORD
+        value: some-password
     YAML
   ]
 
+  timeout = 150
   depends_on = [helm_release.postgres]
 }
