4cc0ac1036e54b9c2df84e376c6136d2242b2759
Tobias Waslowski <tobiaswaslowski@gmail.com>
2024-11-03 18:17:04 +0100

    remove 'required providers' configuration entirely

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,18 +3,6 @@ terraform {
     secret_suffix = "open-mood-tracker-state"
     namespace     = "arc-runner"
   }
-
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-  }
 }
 
 provider "kubernetes" {}
