f931054c6e578638c281c452a0a30be2b002e688
Tobias Waslowski <tobiaswaslowski@gmail.com>
2024-11-07 08:53:54 +0100

    chore: cleanup infra config

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,11 @@
 terraform {
   backend "kubernetes" {
     secret_suffix = "open-mood-tracker-state"
-    config_path = "~/.kube/config"
   }
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
   }
 }
 
