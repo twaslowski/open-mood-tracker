34fe6857b9cf7a17c69dbe3a66a90ef58ab27237
Tobias Waslowski <tobiaswaslowski@gmail.com>
2024-12-22 20:58:14 +0100

    feat: delete last editable message when /resetting

--- src/main/java/de/twaslowski/moodtracker/adapter/telegram/handler/command/ResetHandler.java
+++ src/main/java/de/twaslowski/moodtracker/adapter/telegram/handler/command/ResetHandler.java
@@ -4,23 +4,28 @@ import de.twaslowski.moodtracker.adapter.telegram.MessageUtil;
 import de.twaslowski.moodtracker.adapter.telegram.domain.response.TelegramResponse;
 import de.twaslowski.moodtracker.adapter.telegram.domain.response.TelegramTextResponse;
 import de.twaslowski.moodtracker.adapter.telegram.domain.update.TelegramUpdate;
+import de.twaslowski.moodtracker.adapter.telegram.editable.EditableMarkupMessageService;
 import de.twaslowski.moodtracker.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CancelHandler extends AbstractCommandHandler {
+public class ResetHandler extends AbstractCommandHandler {
 
-  public static final String COMMAND = "/cancel";
+  public static final String COMMAND = "/reset";
   private final UserService userService;
+  private final EditableMarkupMessageService editableMarkupMessageService;
 
-  public CancelHandler(MessageUtil messageUtil, UserService userService) {
+  public ResetHandler(MessageUtil messageUtil, UserService userService, EditableMarkupMessageService editableMarkupMessageService) {
     super(messageUtil);
     this.userService = userService;
+    this.editableMarkupMessageService = editableMarkupMessageService;
   }
 
   @Override
   public TelegramResponse handleUpdate(TelegramUpdate update) {
     userService.resetUserState(update.getChatId());
+    editableMarkupMessageService.deleteMessageForChatId(update.getChatId());
+
     return TelegramTextResponse.builder()
         .chatId(update.getChatId())
         .text(messageUtil.getMessage("command.canceled"))
