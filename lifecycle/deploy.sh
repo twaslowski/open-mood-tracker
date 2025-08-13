2889c63939c6774179b771c4dc0fec120776f935
Tobias Waslowski <tobiaswaslowski@gmail.com>
2025-08-13 14:20:09 +0200

    feat: manage lifecycle with Taskfile

--- lifecycle/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-
-PROJECT_ROOT=$(git rev-parse --show-toplevel)
-export PROJECT_ROOT
-source "$PROJECT_ROOT/lifecycle/common.sh"
-
-deploy
--- lifecycle/stop-environment.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-PROJECT_ROOT=$(git rev-parse --show-toplevel)
-export PROJECT_ROOT
-
-source "$PROJECT_ROOT/lifecycle/common.sh"
-
-stop_environment
