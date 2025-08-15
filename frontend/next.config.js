128e4083487732d1cd7d132728067af2ff16504c
Tobias Waslowski <tobiaswaslowski@gmail.com>
2025-08-15 21:23:39 +0200

    fix(frontend): disable frontend image build temporarily; revert next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const backendUrl = process.env.BACKEND_URL || 'http://localhost:8080';
-
 const nextConfig = {
   eslint: {
     dirs: ['src'],
@@ -9,7 +6,7 @@ const nextConfig = {
   rewrites: async () => [
     {
       source: '/api/:path*',
-      destination: `${backendUrl}/api/:path*`,
+      destination: `${process.env.SERVER_HOST}/api/:path*`,
     },
   ],
 
@@ -47,8 +44,10 @@ const nextConfig = {
 
     return config;
   },
+  env: {
+    TELEGRAM_BOT_NAME: process.env.TELEGRAM_BOT_NAME,
+    SERVER_HOST: process.env.SERVER_HOST,
+  },
 };
 
-module.exports = {
-  nextConfig,
-};
+module.exports = nextConfig;
